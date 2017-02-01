58b614f246e066fec5d313989a5277aae3a6ebb3
Yoshiyuki Hirano <yhirano@aiming-inc.com>
2017-02-01 14:22:36 +0900

    code generate by rails g scaffold Person

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170201034151) do
+ActiveRecord::Schema.define(version: 20170201052208) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "people", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
